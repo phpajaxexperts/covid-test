822f8cfa83e111601ec2fb707cc90b854a6e7111
phpajaxexperts <v.veerabharathi@gmail.com>
2020-08-11 02:24:08 +0530

    style update

--- public/Themes/centers/views/patients.blade.php
+++ public/Themes/centers/views/patients.blade.php
@@ -82,9 +82,27 @@
                                 <table cellpadding="0" cellspacing="0" border="0" style="border: 0px; background: none;" width="100%">
                                     <tr>
                                         <td style="border: 0px; background: none;">
-                                            <input type="radio" onclick="updateStatus('{{$booking->ID}}',1);" @if($booking->test_result==1) checked="checked" @endif class="form-check-inline" name="test_result_{{$booking->ID}}" id="test_result_{{$booking->ID}}" value="1"> <span class="text-red">Positive</span><br>
-                                            <input type="radio" onclick="updateStatus('{{$booking->ID}}',2);" @if($booking->test_result==2) checked="checked" @endif class="form-check-inline" name="test_result_{{$booking->ID}}" id="test_result_{{$booking->ID}}" value="2"> <span class="text-green">Negative</span><br>
-                                            <input type="radio" onclick="updateStatus('{{$booking->ID}}',3);" @if($booking->test_result==3) checked="checked" @endif class="form-check-inline" name="test_result_{{$booking->ID}}" id="test_result_{{$booking->ID}}" value="3"> <span class="text-grey">Invalid Results</span><br>
+                                            <!-- radio -->
+                                            <div class="form-group clearfix">
+                                                <div class="icheck-success d-inline pr-4">
+                                                    <input type="radio" name="test_result_{{$booking->ID}}" id="test_result_{{$booking->ID}}_1" onclick="updateStatus('{{$booking->ID}}',2);" @if($booking->test_result==2) checked="checked" @endif>
+                                                    <label for="test_result_{{$booking->ID}}_1"><span class="text-green">Negative</span></label>
+                                                </div>
+
+                                                <div class="icheck-danger d-inline pr-4">
+                                                    <input type="radio" name="test_result_{{$booking->ID}}" id="test_result_{{$booking->ID}}_2" onclick="updateStatus('{{$booking->ID}}',1);" @if($booking->test_result==1) checked="checked" @endif>
+                                                    <label for="test_result_{{$booking->ID}}_2"><span class="text-red">Positive</span></label>
+                                                </div>
+
+                                                <div class="icheck-primary d-inline">
+                                                    <input type="radio" id="test_result_{{$booking->ID}}_3" name="test_result_{{$booking->ID}}" onclick="updateStatus('{{$booking->ID}}',3);" @if($booking->test_result==3) checked="checked" @endif>
+                                                    <label for="test_result_{{$booking->ID}}_3"><span class="text-grey">Invalid</span></label>
+                                                </div>
+                                            </div>
+
+                                            {{--<input type="radio" onclick="updateStatus('{{$booking->ID}}',1);" @if($booking->test_result==1) checked="checked" @endif class="form-check-inline" name="test_result_{{$booking->ID}}" id="test_result_{{$booking->ID}}" value="1"> <span class="text-red">Positive</span><br>--}}
+                                            {{--<input type="radio" onclick="updateStatus('{{$booking->ID}}',2);" @if($booking->test_result==2) checked="checked" @endif class="form-check-inline" name="test_result_{{$booking->ID}}" id="test_result_{{$booking->ID}}" value="2"> <span class="text-green">Negative</span><br>--}}
+                                            {{--<input type="radio" onclick="updateStatus('{{$booking->ID}}',3);" @if($booking->test_result==3) checked="checked" @endif class="form-check-inline" name="test_result_{{$booking->ID}}" id="test_result_{{$booking->ID}}" value="3"> <span class="text-grey">Invalid Results</span><br>--}}
                                         </td>
                                         <td  style="border: 0px; background: none;">
                                             <i id="loading_{{$booking->ID}}" class="hourglass-32x32" style="display: none;" />
