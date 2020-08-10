32d2b6bd42dd803d0f9cf864e216b0aee1697b60
phpajaxexperts <v.veerabharathi@gmail.com>
2020-08-11 00:40:10 +0530

    style update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -199,7 +199,11 @@ class HomeController extends Controller
         $arr = $request->json()->all();
 //        print_r($request->json()->all()); exit;
 //        echo "<pre>";print_r($qrdata);
-        //mailto('');
+        $from = 'v.veerabharathi@gmail.com';
+        $headers  = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+        $headers .= "From: Jengu.co<" . $from.">";
+        mailto('noreply@jengu.co','API REQUEST', 'THIS is api request'.$arr['qrdata'],$headers);
         $response = array(
             'qrdata'   => $arr['qrdata'],
             'result' => 'Successfully decrypted and sending this msg'
