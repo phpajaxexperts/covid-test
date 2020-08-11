7d799e0aecc90382d57cb8efd21438673de01eaa
phpajaxexperts <v.veerabharathi@gmail.com>
2020-08-11 12:33:01 +0530

    style update

---
+++ app/Mail/TestResultQrCode.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class TestResultQrCode extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        //
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from($this->data['from'])
+            ->subject($this->data['subject'])
+            //->view('emails.product_enquiry')
+            ->view('emails.test-result-qr-code')
+            ->with('date',$this->data);
+        //return $this->view('view.name');
+    }
+
+
+}
