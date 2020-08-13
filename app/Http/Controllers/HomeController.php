13d3df211018f12dd4e1d9f58d75a4d257d763f9
phpajaxexperts <v.veerabharathi@gmail.com>
2020-08-13 20:19:11 +0530

    style update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -293,7 +293,7 @@ class HomeController extends Controller
             $result = array(
                 'test_result' => $test_result,
                 'name' => $patient->name,
-                'test_date' => date('M d, Y',strtotime($booking->booking_time)).' at '.date('h:s A',strtotime($booking->booking_time)),
+                'test_date' => date('M d, Y',strtotime($booking->booking_time)).' at '.date('h:i A',strtotime($booking->booking_time)),
                 'issuer' => $center->name,
             );
 
