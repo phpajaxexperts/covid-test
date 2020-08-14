4acc357ab18298b5d28f73250b1e3c6ce0771906
phpajaxexperts <v.veerabharathi@gmail.com>
2020-08-14 20:06:18 +0530

    style update

--- public/Themes/covid/views/point-of-entry-test.blade.php
+++ public/Themes/covid/views/point-of-entry-test.blade.php
@@ -574,7 +574,7 @@
                                         <div class="form-group {{ $errors->has('phone') ? 'has-error' : ''}}">
                                             <label for="phone" class="control-label">{{ 'Phone' }}</label>
                                             <div>
-                                                <input class="form-control" name="phone" type="text" id="phone" maxlength="15" value="{{ isset($patient->phone) ? $patient->phone : ''}}" ><div style="font-size: 12px">Ex: Mobile number ignore the '+' sign and country code. Example 013xxxxxxx (Malaysia) or 8xxxxxxx (Singapore).</div>
+                                                <input class="form-control" name="phone" type="text" id="phone" maxlength="15" value="{{ isset($patient->phone) ? $patient->phone : ''}}" ><div style="font-size: 12px">Ex: Mobile number ignore the '+' sign and country code. Example +6013 xxx xxxx (Malaysia) should be entered as 13 xxx xxxx & <br> +65 xxxx xxxx  should be entered as xxxx xxxx (Singapore).</div>
                                                 {!! $errors->first('phone', '<p class="help-block">:message</p>') !!}
                                             </div>
                                         </div>
